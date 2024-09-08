1cb00b963b3185c110361152282f332626a31441
David Wee <rook2pawn@gmail.com>
2024-09-08 00:29:46 -0700

    update

--- addons/fsm-godot/scripts/finite_state_machine.gd
+++ addons/fsm-godot/scripts/finite_state_machine.gd
@@ -2,8 +2,6 @@
 extends Node
 class_name FiniteStateMachine
 
-signal transitionEvent(transitionEventName: String)
-
 ## Initial State that the State Machine will start with
 @export var _initial_state : State
 
@@ -15,23 +13,25 @@ signal transitionEvent(transitionEventName: String)
 var _states : Array[State]
 
 # proxies emits to external agents
-func emit(transitionEventName):
-	transitionEvent.emit(transitionEventName)
-	# query if current state has 
+func emit(transitionEventName: String):
+	_current_state.transitionRequested(transitionEventName)
 
-func onTransitionEvent(transitionEventName):
-	for state in _states:
-		state.transitionRequested(transitionEventName)
+# Generalized function to connect any signal
+func connect_ready_signal(sig : Signal):
+	sig.connect(on_ready_signal_received)
+
+# Example callback for the ready signal
+func on_ready_signal_received():
+	printt("FSM: ON READY SIGNAL RECEIVED")
+	if _initial_state:
+		change_state(_initial_state)
 		
-
 func _ready():
+	# populate states
 	for child in get_children():
 		if child is State:
 			_states.append(child)
-
-	transitionEvent.connect(onTransitionEvent)
-	if _initial_state:
-		change_state(_initial_state)
+	printt("FSM READY")
 
 func _process(delta):
 	if _current_state:
@@ -50,7 +50,7 @@ func change_state(new_state: State):
 	if _current_state is State:
 		# Exit the current state and enter the new one
 		_current_state.exit_state()
-	new_state.enter_state()
+	new_state.enter_state() 
+	#printt("SETTING _CURRENT_STATE TO NEW STATE:", _current_state, new_state)
 	# Set the new state as the current one
 	_current_state = new_state
-
--- addons/fsm-godot/scripts/state.gd
+++ addons/fsm-godot/scripts/state.gd
@@ -6,10 +6,6 @@ class_name State
 
 # The Finite State Machine that this State is a part of. The FSM is always a parent of the State
 @onready var _fsm : FiniteStateMachine = get_parent() as FiniteStateMachine
-
-## The interval at which the transitions are checked
-@export var _check_transition_interval : float = 1
-
 # map of transition trigger event names to transitions.
 var _transitions : Dictionary = {}
 
@@ -18,10 +14,12 @@ func _ready():
 		if child is Transition and child.transition_event_name != null:
 			_transitions[child.transition_event_name] = child
 
-func transitionRequested(transitionEventName):
+func transitionRequested(transitionEventName) -> bool:
 	if (_transitions.has(transitionEventName)):
 		var transition = _transitions[transitionEventName]
 		_fsm.change_state(transition.target_state)
+		return true
+	return false
 
 ## Called when the node enters the scene tree for the first time.
 func enter_state() -> void:
